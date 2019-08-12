27dba59f667662535f421ae30a789fc28a6c78ef
Akhilesh Kumar <2045928+AKKUM@users.noreply.github.com>
2019-08-12 16:33:58 +0100

    [COTCO-732][AK] Implemented API as per provided spec (#1)

--- README.md
+++ README.md
@@ -6,3 +6,30 @@ This is a placeholder README.md for a new repository
 ### License
 
 This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
+
+## Production endpoints
+
+```POST  /register-ncf-stub/ncfdata/submit```
+
+Returns the response code with MRN and description.
+
+```
+Request Body example:
+{
+"MRN": "18GB0000601001EBD1",
+"Office":"GB000011"
+}
+```
+
+> Response statuses: 200
+
+```
+200
+Response body example:
+{
+  "MRN": "18IT02110010006A10",
+  "ResponseCode": 7,
+  "ErrorDescription": "Guarantee not valid"
+}
+
+```
--- app/uk/gov/hmrc/registerncfstub/model/NcfRequestData.scala
+++ app/uk/gov/hmrc/registerncfstub/model/NcfRequestData.scala
@@ -14,17 +14,12 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.registerncfstub.config
+package uk.gov.hmrc.registerncfstub.model
 
-import javax.inject.{Inject, Singleton}
-import play.api.Configuration
-import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+import play.api.libs.json.Json
 
-@Singleton
-class AppConfig @Inject()(config: Configuration, servicesConfig: ServicesConfig) {
+case class NcfRequestData(MRN:String, Office:String)
 
-  val authBaseUrl: String = servicesConfig.baseUrl("auth")
-
-  val auditingEnabled: Boolean = config.get[Boolean]("auditing.enabled")
-  val graphiteHost: String     = config.get[String]("microservice.metrics.graphite.host")
+object NcfRequestData {
+  implicit val formats = Json.format[NcfRequestData]
 }
