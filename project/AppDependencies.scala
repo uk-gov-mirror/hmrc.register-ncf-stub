ac5a8151236d1f5b5c4f85c040b8e9e56c82c029
Mahesh <mahesh@Maheshs-MacBook-Pro.local>
2020-01-08 15:29:53 +0000

    [COTCO-1136] introducing api stub response delays

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,17 +6,17 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "0.42.0"
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.42.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "0.42.0" % Test classifier "tests",
-    "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
-    "com.typesafe.play"       %% "play-test"                % current                 % "test",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it",
-    "uk.gov.hmrc"             %% "hmrctest"                 % "3.9.0-play-26"         % "test, it"
+    "uk.gov.hmrc"            %% "bootstrap-play-26"  % "0.42.0" % Test classifier "tests",
+    "org.scalatest"          %% "scalatest"          % "3.0.8" % "test",
+    "com.typesafe.play"      %% "play-test"          % current % "test",
+    "org.pegdown"            % "pegdown"             % "1.6.0" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % "test, it",
+    "uk.gov.hmrc"            %% "hmrctest"           % "3.9.0-play-26" % "test, it",
+    "com.github.kxbmap"      %% "configs"            % "0.4.4"
   )
 
 }
