c8953f7b733b94430c0f94ce2dc7101c309e1ea9
Peter Hazell <18005888+peter-hazell@users.noreply.github.com>
2020-04-16 12:01:48 +0100

    [RORO-352][PH] Update dependencies (#17)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,10 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.42.0"
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.7.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-play-26"  % "0.42.0" % Test classifier "tests",
     "org.scalatest"          %% "scalatest"          % "3.0.8" % "test",
     "com.typesafe.play"      %% "play-test"          % current % "test",
     "org.pegdown"            % "pegdown"             % "1.6.0" % "test, it",
